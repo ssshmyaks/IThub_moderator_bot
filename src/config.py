f1f5c654ee877003988a088cd949069cd91e2c92
ssshmyaks <153379674+ssshmyaks@users.noreply.github.com>
2024-10-03 18:00:41 +1000

    main

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 BOT_TOKEN = '7503175143:AAF1O7_o7zGPqkgTBnepcz9swvwbAhjEs-8'
 password = 'sigma'
 admin = '825627855'
-chats = ['-1002196623720', '']
+chat = '941235969'
 database_path = 'src/database/database.db'
